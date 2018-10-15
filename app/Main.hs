969a911492dd7f8bcb497bd7ead0eef9d7692fce
Jan Christiansen <jan.christiansen@hs-flensburg.de>
2018-10-15 20:51:48 +0200

    Fix problem with byte order mark

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,15 @@
 
 module Main where
 
-import Control.Lens
-import Network.Wreq
-import Text.CSV
-import Data.ByteString.Lazy.Char8
+import           Control.Lens
+import           Data.ByteString.Lazy.Char8 hiding (drop)
+import           Network.Wreq
+import           Prelude
+import           Text.CSV
 
 url = "http://www.bundesbank.de/cae/servlet/StatisticDownload?tsId=BBEX3.D.XAU.EUR.EA.AC.C04&its_csvFormat=en&its_fileFormat=csv&mode=its"
 
 main = do
   request <- get url
-  parseCSVTest (unpack (request ^. responseBody))
+  parseCSVTest (drop 3 (unpack (request ^. responseBody)))
   return ()
