ad6249111f20efa57d52e1dd542de1c033098390
Sandra Dylus <ichistmeinname@web.de>
2018-10-28 20:28:58 +0100

    Add error handling and exit codes
    Add dependency for Network.HTTP.Client.
    Import Network.HTTP.Client and Control.Exception to catch http-request errors.
    Yield exit codes for failures and success.

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,15 @@
 
 module Main where
 
+import qualified Control.Exception as E
 import           Control.Lens
 import           Data.Aeson
 import           Data.ByteString.Lazy.Char8 hiding (any, drop, map)
 import           Data.Maybe
 import           GHC.Generics
+import           Network.HTTP.Client               (HttpException(..), HttpExceptionContent(..))
 import           Network.Wreq
+import           System.Exit                       (exitFailure, exitSuccess)
 import           Text.CSV
 import           Text.Read                  hiding (get)
 
@@ -17,18 +20,29 @@ data Item = Item {date  :: String, value :: Float} deriving (Generic, Show)
 instance ToJSON Item where
   toEncoding = genericToEncoding defaultOptions
 
+actionWithCatch :: (String -> IO (Response ByteString))
+                -> String
+                -> IO (Either HttpExceptionContent (Response ByteString))
+actionWithCatch action url =
+  (action url >>= return . Right) `E.catch` (return . handler)
+ where
+  handler (HttpExceptionRequest _ httpException) = Left httpException
+
 main = do
-  delete backendUrl
-  csvRequest <- get csvUrl
-  let lines = drop 3 (unpack (csvRequest ^. responseBody))
-  case parseCSV "gold.csv" lines of
-    Left error  -> print error
-    Right lists -> do
-      let payload = encode (catMaybes (map toItem lists))
-      let options = defaults & header "Content-Type" .~ ["application/json"]
-      postRequest <- postWith options backendUrl payload
-      print $ postRequest ^. responseStatus . statusCode
-  return ()
+  eDeleteRequest <- actionWithCatch delete backendUrl
+  either errorCase (const mkRequest) eDeleteRequest
+ where
+  mkRequest = do
+    csvRequest <- get csvUrl
+    let lines = drop 3 (unpack (csvRequest ^. responseBody))
+    case parseCSV "gold.csv" lines of
+      Left error  -> print error
+      Right lists -> do
+        let payload = encode (catMaybes (map toItem lists))
+        let options = defaults & header "Content-Type" .~ ["application/json"]
+        ePostRequest <- actionWithCatch (\url -> postWith options url payload) backendUrl
+        either errorCase (const exitSuccess) ePostRequest
+  errorCase exception = print exception >> exitFailure
 
 toItem :: [String] -> Maybe Item
 toItem [x, y, _] = fmap (\value -> Item x value) (readMaybe y :: Maybe Float)
